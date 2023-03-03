692f95fce509ae9e0de57674175558b0dfb1eb50
yefen123 <yefenblog123@163.com>
2023-03-03 01:21:54 +0000

    modify

--- src/jobs/example5Jobs.groovy
+++ src/jobs/example5Jobs.groovy
@@ -1,31 +1,17 @@
-import com.dslexample.util.StepsUtil
-
-String basePath = 'example5'
-
-folder(basePath) {
-    description 'This example shows how to pull out common components into static methods.'
-}
-
-job("$basePath/static-method-example1") {
-
-    steps {
-        gradle {
-            useWrapper true
-            tasks 'clean test'
-            switches '''
-                -Dhttp.proxyHost=proxy.example.com
-                -Dhttps.proxyHost=proxy.example.com
-                -Dhttp.proxyPort=80
-                -Dhttps.proxyPort=80
-            '''
+job('email_example') {
+    publishers {
+        extendedEmail {
+            recipientList('fen.ye@tietoevry.com')
+            triggers {
+                success {
+                    attachBuildLog(true)
+                    subject('Subject')
+                    content('Body')
+                    sendTo {
+                        recipientList()
+                    }
+                }
+            }
         }
     }
 }
-
-job("$basePath/static-method-example2") {
-
-    steps {
-        StepsUtil.proxiedGradle delegate, 'clean test'
-    }
-}
-
